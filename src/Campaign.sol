30f8f1bf3e6149a7e6158bdad79be9476a4678b4
mehakb7 <mehak@founderandlightning.com>
2024-09-02 22:18:52 +0530

    added s_  prefix for  storage variable

--- src/Campaign.sol
+++ src/Campaign.sol
@@ -16,17 +16,17 @@ contract Campaign {
         FLEXIBLE
     }
 
-    address private owner;
-    bool private hasCampaignEnded;
-    bool private hasCampaignSucceed;
-    CampaignType private campaignType = CampaignType.FIXED;
+    address private s_owner;
+    bool private s_hasCampaignEnded;
+    bool private s_hasCampaignSucceed;
+    CampaignType private s_campaignType = CampaignType.FIXED;
 
-    uint256 private goal;
-    uint256 private endDate;
-    uint256 private startTime;
-    uint256 private totalDonation;
+    uint256 private s_goal;
+    uint256 private s_endDate;
+    uint256 private s_startTime;
+    uint256 private s_totalDonation;
 
-    string private cid;
+    string private s_cid;
 
     mapping(address => uint) private donars;
     address[] private donarAddress;
@@ -38,31 +38,31 @@ contract Campaign {
         address _owner,
         CampaignType _campaignType
     ) {
-        cid = _cid;
-        goal = _goal;
-        endDate = block.timestamp + (_daysToend * 1 days);
-        campaignType = _campaignType;
-        owner = _owner;
-        startTime = block.timestamp;
+        s_cid = _cid;
+        s_goal = _goal;
+        s_endDate = block.timestamp + (_daysToend * 1 days);
+        s_campaignType = _campaignType;
+        s_owner = _owner;
+        s_startTime = block.timestamp;
     }
 
     modifier isWithinDate() {
-        if (block.timestamp > endDate) {
-            hasCampaignEnded = true;
+        if (block.timestamp > s_endDate) {
+            s_hasCampaignEnded = true;
             revert CampaignHasEnded();
         }
         _;
     }
 
     modifier onlyOwner() {
-        if (msg.sender != owner) {
+        if (msg.sender != s_owner) {
             revert NotAutorized();
         }
         _;
     }
 
     function deposite() public payable isWithinDate {
-        if (hasCampaignEnded) {
+        if (s_hasCampaignEnded) {
             revert CampaignHasEnded();
         }
         if (donars[msg.sender] == 0) {
@@ -70,31 +70,31 @@ contract Campaign {
         }
         donars[msg.sender] += msg.value;
         emit DonationReceived(msg.sender, msg.value);
-        totalDonation += msg.value;
+        s_totalDonation += msg.value;
     }
 
     function withdraw() public onlyOwner {
-        if (!hasCampaignEnded) {
+        if (!s_hasCampaignEnded) {
             revert CampaignStillRunning();
         }
 
-        if (!hasCampaignSucceed && campaignType == CampaignType.FIXED) {
+        if (!s_hasCampaignSucceed && s_campaignType == CampaignType.FIXED) {
             revert CampaignFailed();
         }
 
-        (bool success, ) = address(owner).call{value: address(this).balance}(
+        (bool success, ) = address(s_owner).call{value: address(this).balance}(
             ""
         );
         require(success);
     }
 
     function withdrawByDoner() public {
-        if (!hasCampaignEnded) {
+        if (!s_hasCampaignEnded) {
             revert CampaignStillRunning();
         }
 
         require(
-            campaignType == CampaignType.FLEXIBLE,
+            s_campaignType == CampaignType.FLEXIBLE,
             "can't withdraw money from fixed campagin"
         );
 
@@ -107,12 +107,12 @@ contract Campaign {
     }
 
     function endCampaign() public onlyOwner {
-        hasCampaignEnded = true;
-        if (goal <= address(this).balance) {
-            hasCampaignSucceed = true;
+        s_hasCampaignEnded = true;
+        if (s_goal <= address(this).balance) {
+            s_hasCampaignSucceed = true;
         }
 
-        emit CampaginEnded(hasCampaignSucceed);
+        emit CampaginEnded(s_hasCampaignSucceed);
     }
 
     receive() external payable {}
@@ -122,15 +122,15 @@ contract Campaign {
     }
 
     function getGoal() external view returns (uint256) {
-        return goal;
+        return s_goal;
     }
 
     function getTotalDonation() external view returns (uint256) {
-        return totalDonation;
+        return s_totalDonation;
     }
 
     function getCId() external view returns (string memory) {
-        return cid;
+        return s_cid;
     }
 
     function getCampiagnInfo()
@@ -139,13 +139,13 @@ contract Campaign {
         returns (uint256, uint256, uint256, bool, bool, address, CampaignType)
     {
         return (
-            goal,
-            totalDonation,
-            endDate,
-            hasCampaignEnded,
-            hasCampaignSucceed,
-            owner,
-            campaignType
+            s_goal,
+            s_totalDonation,
+            s_endDate,
+            s_hasCampaignEnded,
+            s_hasCampaignSucceed,
+            s_owner,
+            s_campaignType
         );
     }
 }
